74e587e6f2240e6153ad5606e3f9e3b92d2a965d
rainbowdango <minhkhoi1026@gmail.com>
2021-08-17 18:29:29 +0700

    complete exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,9 +6,28 @@ const VoteButton = ({onClick, text}) => {
     {text}
   </button>
   )
-}
+};
 
-const Summary = ({name, val}) => (<p>{name}: {val}</p>)
+const Summary = ({name, val}) => (<p>{name}: {val}</p>);
+
+const Statistics = (props) => {
+  const {good, neutral, bad} = props
+  const numFeedback = () => (good + neutral + bad);
+  const avgScore = () => ((good - bad)/numFeedback());
+  const positiveRate = () => (100 * good/numFeedback());
+
+  return (
+  <div>
+    <h1>Statistics</h1>
+    <Summary name="good" val={good}/>
+    <Summary name="neutral" val={neutral}/>
+    <Summary name="bad" val={bad}/>
+    <Summary name ="all" val={numFeedback()}/>
+    <Summary name ="average" val={avgScore()}/>
+    <p>positive: {positiveRate()}% </p>
+  </div>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -18,9 +37,6 @@ const App = () => {
   const voteGood = () => setGood(good + 1);
   const voteNeutral = () => setNeutral(neutral + 1);
   const voteBad = () => setBad(bad + 1);
-  const numFeedback = () => (good + neutral + bad);
-  const avgScore = () => ((good - bad)/numFeedback());
-  const positiveRate = () => (100 * good/numFeedback());
 
   return (
     <div>
@@ -28,15 +44,9 @@ const App = () => {
       <VoteButton onClick = {voteGood} text="good"/>
       <VoteButton onClick = {voteNeutral} text="neutral"/>
       <VoteButton onClick = {voteBad} text="bad"/>
-      <h1>Statistics</h1>
-      <Summary name="good" val={good}/>
-      <Summary name="neutral" val={neutral}/>
-      <Summary name="bad" val={bad}/>
-      <Summary name ="all" val={numFeedback()}/>
-      <Summary name ="average" val={avgScore()}/>
-      <p>positive: {positiveRate()}% </p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
-  )
+  );
 }
 
 export default App

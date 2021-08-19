96c22b493dd47fc73a4152ae30a6b91bd1db089f
rainbowdango <minhkhoi1026@gmail.com>
2021-08-19 22:25:53 +0700

    complete exercise 2.12

--- part2/restcountries/src/App.js
+++ part2/restcountries/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
+const Country = ({country}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{country.name}</h2>
+      <ul>
+        <li>Capital name: {country.capital}</li>
+        <li>Population: {country.population} people</li>
+      </ul>
+      <h3>Languages</h3>
+      <ul>
+        {country.languages.map(
+          language => (<li key={language.iso639_2}>{language.name}</li>)
+        )}
+      </ul>
+      <img 
+      src={country.flag}
+      alt={`${country.name} flag`}
+      width="150px"
+      />
     </div>
+  )
+}
+
+const CountriesDisplay = ({countries}) => {
+  // if more than 10 countries founded then return this
+  if (countries.length > 10)
+    return (
+      <p>Too many matches, specify your country</p>
+    )
+  // if there only exactly 1 country founded then return this
+  if (countries.length === 1) {
+    return (
+      <Country country={countries[0]}/>
+    )
+  }
+    
+  // other wise return this
+  return (
+    <ul>
+      {countries.map(
+        (country) => (<li key={country.name}>{country.name}</li>)
+      )}
+    </ul>
+  )
+}
+
+const App = () => {
+  const [query, setQuery] = useState('')
+  const [countries, setCountries] = useState([])
+
+  //fetch data from RESTcountries server
+  const data_hook = () => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then( response => setCountries(response.data) )
+  }
+  useEffect(data_hook, [])
+  const filteredCountries = countries.filter(
+    country => country.name.toLowerCase().includes(query.toLowerCase())
+    )
+
+  const handleQueryChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  return (
+  <div>
+    <p>
+      Find countries <input value={query} onChange={handleQueryChange}/>
+    </p>
+    <CountriesDisplay countries={filteredCountries}/>
+  </div>
   );
 }
 

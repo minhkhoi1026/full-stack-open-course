4611d31a6039401374d9caea4d6ebb2c833ec802
rainbowdango <minhkhoi1026@gmail.com>
2021-08-23 20:52:59 +0700

    seperate mongodb into module in example 3b

---
+++ part3/part3/models/note.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+// connect to database
+console.log('connecting to', url)
+mongoose.connect(url, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false, 
+    useCreateIndex: true 
+})
+.then( result => console.log('connected to mongoDB'))
+.catch( error => console.log('error connecting to MongoDB:', error.message))
+// define document schema
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+// export constructor compiled from schema
+module.exports = mongoose.model('Note', noteSchema)

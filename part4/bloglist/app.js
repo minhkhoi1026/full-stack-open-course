9d83c09c4ba5fa22959a7b59208456b7cd14af14
rainbowdango <minhkhoi1026@gmail.com>
2021-08-27 18:36:57 +0700

    bloglist: seperate app module

---
+++ part4/bloglist/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+console.log(`connecting to`, mongoUrl)
+mongoose.connect(mongoUrl , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(() => logger.info("connected to server!"))
+.catch(error => logger.error("connect failed, error info: ", error))
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,48 +1,10 @@
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const app = require('./app')
 
-const mongoUrl = config.MONGODB_URI
-console.log(`connecting to`, mongoUrl)
-mongoose.connect(mongoUrl , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(() => logger.info("connected to server!"))
-.catch(error => logger.error("connect failed, error info: ", error))
+server = http.createServer(app)
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })

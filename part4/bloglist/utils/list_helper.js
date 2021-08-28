c157be465164d54da846870224742412d27ad5b7
rainbowdango <minhkhoi1026@gmail.com>
2021-08-28 09:46:24 +0700

    complete exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = (blogs) => {
   return 1
 }
+const totalLikes = (blogs) => {
+  const calculateSum = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.reduce(calculateSum, 0)
+}
 
-module.exports = {dummy}
+module.exports = {dummy, totalLikes}

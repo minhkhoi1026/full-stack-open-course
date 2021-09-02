c93ef5fd63cc324e6b09e7f3c6c19de938ca0b63
rainbowdango <minhkhoi1026@gmail.com>
2021-09-02 10:34:08 +0700

    complete exercise 4.17

--- part4/bloglist/controllers/user.js
+++ part4/bloglist/controllers/user.js
@@ -3,7 +3,8 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+        .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
 
   response.json(users)
 })
@@ -20,7 +21,8 @@ usersRouter.post('/', async (request, response) => {
   const newUser = new User({
     username: body.username,
     name: body.name,
-    passwordHash
+    passwordHash,
+    notes: []
   })
 
   const resultUser = await newUser.save()

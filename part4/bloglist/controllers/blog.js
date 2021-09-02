f4948f9cf2c9775a279902a5c1d1d64f850d5194
rainbowdango <minhkhoi1026@gmail.com>
2021-09-02 15:35:42 +0700

    complete exercise 4.19

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -8,10 +10,23 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer '))
+    return authorization.substring(7)
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  
+  const token = getTokenFrom(request)
 
-  const user = (await User.find({}))[0]
+  const decodedToken = jwt.verify(token, config.KEY)
+  if (!decodedToken || !decodedToken.id)
+    return response.status(401).json({ error: 'token missing or invalid' })
+
+  const user = await User.findById(decodedToken.id)
   const newBlog = new Blog({
     url: body.url,
     title: body.title,

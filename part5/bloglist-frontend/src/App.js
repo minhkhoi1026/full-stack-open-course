e954c330f4c958fc04f2618a1147568663c45039
rainbowdango <minhkhoi1026@gmail.com>
2021-09-08 10:23:41 +0700

    complete exercise 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -122,9 +122,11 @@ const App = () => {
           <BlogForm createBlog={createBlog}/>
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLikeClick={handleLikeClick} />
-      )}
+      {
+      blogs
+      .sort((a, b) => b.likes - a.likes)
+      .map(blog => <Blog key={blog.id} blog={blog} handleLikeClick={handleLikeClick}/>)
+      }
     </div>
   )
 }
